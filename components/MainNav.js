fb688675d2be832876be8df7e8e0a0598a847ea6
pandaconnect1 <info.pandaconnect@gmail.com>
2025-11-24 01:29:01 +0200

    Create MainNav.js

---
+++ components/MainNav.js
@@ -0,0 +1,7 @@
+export default function MainNav() {
+  return (
+    <nav className="py-4 px-6 bg-gray-900 border-b border-gray-700">
+      <h1 className="text-xl font-semibold">Pantavion One</h1>
+    </nav>
+  );
+}
