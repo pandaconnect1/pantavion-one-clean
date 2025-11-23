ef2ad66b1b0cc11e0fedb030d88ad8fc6a3ee830
pandaconnect1 <info.pandaconnect@gmail.com>
2025-11-23 23:25:03 +0200

    Change body background color to black

--- app/app/layout.js
+++ app/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-pantavion-dark text-white">
+      <body className="min-h-screen bg-black text-white">
         {children}
       </body>
     </html>
