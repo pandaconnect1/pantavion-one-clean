b13014ef52ab3a32a6039fed7a836e69eecaccd5
pandaconnect1 <info.pandaconnect@gmail.com>
2025-11-24 01:28:19 +0200

    Delete components directory

--- components/MainNav.js
+++
@@ -1,7 +0,0 @@
-export default function MainNav() {
-  return (
-    <nav className="py-4 px-6 bg-gray-900 border-b border-gray-700">
-      <h1 className="text-xl font-semibold">Pantavion One</h1>
-    </nav>
-  );
-}
