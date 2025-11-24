cc22d63ed7fde9f846374dcda02aca62ad45b043
pandaconnect1 <info.pandaconnect@gmail.com>
2025-11-24 23:32:07 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,15 +5,12 @@ import SectionOverview from "../components/SectionOverview";
 
 export default function HomePage() {
   return (
-    <main className="min-h-screen flex flex-col">
-      {/* Global Navigation */}
-      <MainNav />
-
-      {/* Κεντρικό Hero */}
-      <Hero />
-
-      {/* Πλέγμα με τα sections */}
-      <SectionOverview />
+    <main className="min-h-screen bg-black text-gray-100">
+      <div className="max-w-5xl mx-auto px-6 py-10 space-y-12">
+        <MainNav />
+        <Hero />
+        <SectionOverview />
+      </div>
     </main>
   );
 }
