02ffda6b89a3eea810f4566a8726ec9768a57b24
pandaconnect1 <info.pandaconnect@gmail.com>
2025-11-26 12:20:01 +0200

    Update page.js

--- app/compass/page.js
+++ app/compass/page.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { motion } from "framer-motion";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent } from "../../components/ui/card";
 
 const sections = [
   "World Map & Regions",
